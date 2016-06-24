435e5e4c5719ec3419694611030390bd84648f40
Geonmo <ry840901@gmail.com>
2016-06-24 13:21:46 +0900

    Standalone code

--- JetTestAna.cpp
+++ JetTestAna.cpp
@@ -3,6 +3,7 @@
 #include "external/ExRootAnalysis/ExRootTreeReader.h"
 #include "external/ExRootAnalysis/ExRootResult.h"
 #include <TClonesArray.h>
+#include <TH1.h>
 using namespace std;
 
 class ExRootTreeReader;
@@ -10,6 +11,14 @@ class ExRootResult;
 
 struct TestPlots
 {
+  TH1 *fgenJpsiPT;
+  TH1 *fgenJpsiEta;
+  TH1 *fgenJpsiPhi;
+  TH1 *fgenJpsiM;
+
+  TH1 *fJpsiPT;
+  TH1 *fJpsiEta;
+  TH1 *fJpsiPhi;
 
 };
 
@@ -17,10 +26,49 @@ struct TestPlots
 
 void BookHistograms(ExRootResult *result, TestPlots *plots)
 {
+  plots->fgenJpsiPT = result->AddHist1D(
+    "genjpsi_pt", "p_{T}",
+    "p_{T}", "Entries",
+    100, 0., 200. );
+  plots->fgenJpsiEta = result->AddHist1D(
+    "genjpsi_eta", "eta",
+    "eta", "number of jpsis",
+    100, -5., 5. );
+  plots->fgenJpsiPhi = result->AddHist1D(
+    "genjpsi_phi", "phi",
+    "phi", "number of jpsis",
+    100, -3.14, +3.14 );
+  plots->fgenJpsiM = result->AddHist1D(
+    "genjpsi_mass", "mass",
+    "mass", "number of jpsis",
+    100, 0., 5. );
+
+  plots->fJpsiPT = result->AddHist1D(
+    "jpsi_pt", "p_{T}",
+    "p_{T}", "number of Jpsi",
+    100, 0, 200 );
+  plots->fJpsiEta = result->AddHist1D(
+    "jpsi_eta", "eta",
+    "eta", "number of jpsis",
+    100, -5, 5 );
+  plots->fJpsiPhi = result->AddHist1D(
+    "jpsi_phi", "phi",
+    "phi", "number of jpsis",
+    100, -TMath::Pi(), TMath::Pi() );
+
 }
 
 //------------------------------------------------------------------------------
-
+int FindMother(TClonesArray* genParticles, GenParticle* base, int motherPID )
+{
+  int value = 0;
+  while(base->M1 != -1 ) {
+    GenParticle* mother = (GenParticle*) genParticles->At(base->M1);
+    if ( abs(base->PID) == motherPID ) value = base->PID;
+    base = mother;
+  }
+  return value;
+}
 void AnalyseEvents(ExRootTreeReader *treeReader, TestPlots *plots)
 {
   TClonesArray *branchParticle = treeReader->UseBranch("Particle");
@@ -49,6 +97,23 @@ void AnalyseEvents(ExRootTreeReader *treeReader, TestPlots *plots)
     treeReader->ReadEntry(entry);
     // Loop over all jets in event
     std::cout<<"Event : "<<entry<<std::endl;
+    for (int  i =0 ; i < branchParticle->GetEntriesFast() ; ++i) {
+      particle = (GenParticle*) branchParticle->At(i);
+      bool isJpsiExisted = false;
+      if ( abs( particle->PID ) == 443 ) {
+        GenParticle* base  = particle;
+        int motherPID = FindMother( branchParticle, base, 6) ;
+        if ( abs(motherPID) == 6) isJpsiExisted = true;
+      }
+      else continue;
+      if ( isJpsiExisted ) {
+        plots->fgenJpsiPT->Fill(particle->PT);
+        plots->fgenJpsiEta->Fill(particle->Eta);
+        plots->fgenJpsiPhi->Fill(particle->Phi);
+        plots->fgenJpsiM->Fill(particle->Mass);
+      } 
+    }
+
     for(int i = 0; i < branchJet->GetEntriesFast(); ++i)
     {
       Jet* jet = (Jet*) branchJet->At(i);
@@ -109,7 +174,6 @@ void PrintHistograms(ExRootResult *result, TestPlots *plots)
 int main()
 {
   const char* inputFile = "Delphes.root";
-  //gSystem->Load("libDelphes");
 
   TChain *chain = new TChain("Delphes");
   chain->Add(inputFile);
