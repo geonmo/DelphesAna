da27df735d55ac3fb7012c3e2ac0accb3f16931c
Geonmo <ry840901@gmail.com>
2016-06-28 00:07:10 +0900

    move files to production dir.

--- production/setupProd.sh
+++ production/setupProd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+eval `scramv1 runtime -sh`
 wget http://cp3.irmp.ucl.ac.be/downloads/Delphes-3.3.2.tar.gz
 tar -zxf Delphes-3.3.2.tar.gz
 
@@ -6,3 +7,6 @@ cp DelphesCMSFWLite.cpp Delphes-3.3.2/reader
 
 cd Delphes-3.3.2
 make -j 20
+
+cd -
+tar -czvf delphesProd.tar.gz Delphes-3.3.2/
