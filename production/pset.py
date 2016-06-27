46f9befa98e1506cf4a59d2f49c34895f83c4643
Geonmo <ry840901@gmail.com>
2016-06-28 00:50:47 +0900

    Update 2

--- production/pset.py
+++ production/pset.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 process = cms.Process("Delphes")
-#process.source = cms.Source("EmptySource")
-print process.source.fileNames
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
+
+print process.source.fileNames = ['/store/user/geonmo/TTLL13TeV_AODSIM/AODSIM.root']
