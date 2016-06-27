498ee01959f3bd651f58c515b241d40d981c17b2
Geonmo <ry840901@gmail.com>
2016-06-28 00:35:53 +0900

    update.

--- production/crab.py
+++ production/crab.py
@@ -5,7 +5,7 @@ config.General.transferOutputs = True
 
 import os
 index = 1
-baseName = 'UserScriptTest'
+baseName = 'DelphesProduction'
 # if it finds crab_UserScriptTest1/, it will use crab_UserScriptTest2/ automatically!
 while os.path.isdir("crab_%s%s" % (baseName, index)):
     index += 1
@@ -21,13 +21,13 @@ config.JobType.outputFiles = ['Delphes.root']
 config.JobType.scriptExe = 'runDelphes.sh'
 
 config.section_('Data')
+config.Data.inputDataset = '/TTTo2L2Nu_13TeV-powheg/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext1-v1/MINIAODSIM'
 config.Data.publication = False
 config.Data.totalUnits = 1
 config.Data.unitsPerJob = 1
-config.Data.splitting = 'EventBased'
+config.Data.splitting = 'FileBased'
 config.Data.ignoreLocality = True
 
 config.section_('User')
 config.section_('Site')
-config.Site.storageSite = 'T2_US_UCSD'
-config.Site.whitelist = ['T2_US_Caltech','T2_US_Florida', 'T2_US_MIT', 'T2_US_Nebraska', 'T2_US_Purdue', 'T2_US_UCSD', 'T2_US_Vanderbilt', 'T2_US_Wisconsin']
+config.Site.storageSite = 'T3_KR_KISTI'
--- production/runDelphes.sh
+++ production/runDelphes.sh
@@ -3,4 +3,16 @@ eval `scramv1 runtime -sh`
 
 echo "This is job number $1"
 
-Delphes3.3.2/DelphesCMSFWLite 
+tar -zxvf delphesProd.tar.gz 
+chmod +x Delphes3.3.2/DelphesCMSFWLite 
+
+
+echo "========= Acquire input filename ========="
+inputfile=`python -c "import PSet; print PSet.Process.source.fileNames[0]"`
+echo $inputfile
+
+pfnInput=`edmFileUtil -d $inputfile`
+
+Delphes3.3.2/DelphesCMSFWLite Delphes-3.3.2/cards/delphes_card_CMS_PileUp.tcl Delphes.root $inputfile 
+
+cmsRun -j FrameworkJobReport.xml -p PSet.py
