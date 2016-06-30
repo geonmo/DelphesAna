5546f5dde60907e1faa112feb17d06bad904eaff
Geonmo <ry840901@gmail.com>
2016-06-30 17:26:03 +0900

    Missing "s"

--- production/setupProd.sh
+++ production/setupProd.sh
@@ -3,7 +3,7 @@ eval `scramv1 runtime -sh`
 wget http://cp3.irmp.ucl.ac.be/downloads/Delphes-3.3.2.tar.gz
 tar -zxf Delphes-3.3.2.tar.gz
 
-cp DelphesCMSFWLite.cpp Delphes-3.3.2/reader 
+cp DelphesCMSFWLite.cpp Delphes-3.3.2/readers 
 
 cd Delphes-3.3.2
 make -j 20
