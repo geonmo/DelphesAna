2d3e3764a582e13ed6f36b4b756647029e2db36c
Geonmo RYU <geonmo@gate2.sscc.uos.ac.kr>
2016-07-20 23:51:22 +0900

    Reduce event loop

--- production/pset_delphes.py
+++ production/pset_delphes.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 process = cms.Process("Delphes")
-process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
+process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.source.fileNames = ['/store/user/geonmo/TTLL13TeV_AODSIM/AODSIM.root']
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
+process.source.fileNames = ['file:/pnfs/user/geonmo/CMSSW_8_1_0_pre8/src/DelphesAna/production/00491874-02FA-E511-BCE8-008CFA197454.root']
+process.MessageLogger.cerr.FwkReport.reportEvery = 5000
