6e03fb4c495cb0a0da4bd2e717e74e3bf6e45db5
Geonmo Ryu <geonmo@cern.ch>
2016-10-03 18:37:07 +0900

    Add FWError message.

--- production/runDelphes.sh
+++ production/runDelphes.sh
@@ -10,13 +10,42 @@ chmod +x delphesCode/DelphesCMSFWLite
 cp delphesCode/MinBias.pileup .
 
 echo "========= Acquire input filename ========="
-inputfile=`python -c "import PSet; print PSet.process.source.fileNames[0]"`
-#inputfile=`python -c "import pset_delphes; print pset_delphes.process.source.fileNames[0]"`
+#inputfile=`python -c "import PSet; print PSet.process.source.fileNames[0]"`
+inputfile=`python -c "import pset_delphes; print pset_delphes.process.source.fileNames[0]"`
 echo $inputfile
 
 pfnInput=`edmFileUtil -d $inputfile`
 echo $pfnInput
 delphesCode/DelphesCMSFWLite delphes_card_CMSPU_mod_noTauTagging.tcl Delphes.root $pfnInput
 
-cmsRun -j FrameworkJobReport.xml -p PSet.py
-#cmsRun -j FrameworkJobReport.xml -p pset_delphes.py
+if [ $? != 0 ] 
+then
+exitCode=$?
+exitMessage="My arbitrary exit message"
+errorType="My arbitrary error type"
+  if [ -e FrameworkJobReport.xml ]
+  then
+cat << EOF > FrameworkJobReport.xml.tmp
+<FrameworkJobReport>
+<FrameworkError ExitStatus="$exitCode" Type="$errorType" >
+$exitMessage
+</FrameworkError>
+EOF
+tail -n+2 FrameworkJobReport.xml >> FrameworkJobReport.xml.tmp
+mv FrameworkJobReport.xml.tmp FrameworkJobReport.xml
+  else
+cat << EOF > FrameworkJobReport.xml
+<FrameworkJobReport>
+<FrameworkError ExitStatus="$exitCode" Type="$errorType" >
+$exitMessage
+</FrameworkError>
+</FrameworkJobReport>
+EOF
+fi
+else 
+echo "Successfully prod."
+fi
+
+
+#cmsRun -j FrameworkJobReport.xml -p PSet.py
+cmsRun -j FrameworkJobReport.xml -p pset_delphes.py
