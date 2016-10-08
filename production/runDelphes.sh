77980b3bd9a102514add860c668d8042209fceb2
Geonmo Ryu <geonmo@cern.ch>
2016-10-08 09:06:54 +0900

    update runDelphes.sh

--- production/runDelphes.sh
+++ production/runDelphes.sh
@@ -4,14 +4,14 @@ pwd
 echo "This is job number $1"
 
 echo "========= Acquire input filename ========="
-inputfile=`python -c "import PSet; print PSet.process.source.fileNames[0]"`
-#inputfile=`python -c "import pset_delphes; print pset_delphes.process.source.fileNames[0]"`
+
+if [ -e PSet.py ]; then
+PYCFG='PSet'
+else
+PYCFG='pset_delphes'
+fi
+inputfile=`python -c "import ${PYCFG}; print ${PYCFG}.process.source.fileNames[0]"`
 echo $inputfile
-
-cp delphes_card_CMSPU_mod.tcl $1
-
-mkdir $1
-cd $1
 
 tar -zxf delphesProd.tar.gz
 
@@ -22,14 +22,15 @@ cp delphesCode/MinBias.pileup .
 
 pfnInput=`edmFileUtil -d $inputfile`
 echo $pfnInput
-delphesCode/DelphesCMSFWLite delphes_card_CMSPU_mod.tcl Delphes.root $pfnInput
+delphesCode/DelphesCMSFWLite delphes_card_CMSnoPU_noTauTagging.tcl Delphes.root $pfnInput
 jobFin=$?
 
+cmsRun -j FrameworkJobReport.xml -p ${PYCFG}.py
 if [ "$jobFin" -ne 0 ]
 then
   echo "delphes job failed"
   exitCode=8028
-  exitMessage="Something is wrong during delphes simulation"
+  exitMessage="delphes simulation failed"
   errorType="File open error or logical error"
 if [ -e FrameworkJobReport.xml ]
 then
@@ -53,8 +54,3 @@ fi
 fi
 
 
-cd ..
-cmsRun -j FrameworkJobReport.xml -p PSet.py
-#cmsRun -j FrameworkJobReport.xml -p pset_delphes.py
-cp $1/Delphes.root .
-rm -rf $1
