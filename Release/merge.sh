e0dd65e309acbb8630f84b0d1b0b50c3b240e6f1
Geonmo <ry840901@gmail.com>
2016-10-27 16:21:06 +0900

    Add merge dir and submit for JKPS

--- Release/merge.sh
+++ Release/merge.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
 eval `scramv1 runtime -sh`
-TTLLDIR=`ls -l | grep TTLL | egrep -v filelist | tail -n 1 | awk '{print $9}'`
-DY50DIR=`ls -l | grep DY50 | egrep -v filelist | tail -n 1 | awk '{print $9}'`
-DY10to50DIR=`ls -l | grep DY10to50 | egrep -v filelist | tail -n 1 | awk '{print $9}'`
-echo $TTLLDIR
-hadd TTLL.root ${TTLLDIR}/ANA*/ANA*.root
-echo $DY50DIR
-hadd DY50.root ${DY50DIR}/ANA*/ANA*.root
-echo $DY10to50DIR
-hadd DY10to50.root ${DY10to50DIR}/ANA*/ANA*.root
+TTLLDIR=`ls -td  TTLL*/ | head -n 1`
+DY1=`ls -td DY10to50*/ | head -n 1 | cut -d'/' -f1`  
+DY2=`ls -td DY50*/ | head -n 1 | cut -d'/' -f1`  
+ST1=`ls -td ST_tW_top*/ | head -n 1 | cut -d'/' -f1`  
+ST2=`ls -td ST_tW_antitop*/ | head -n 1 | cut -d'/' -f1`  
+TMASS1=`ls -td TT_mass_1665*/ | head -n 1 | cut -d'/' -f1`  
+TMASS2=`ls -td TT_mass_1695*/ | head -n 1 | cut -d'/' -f1`  
+TMASS3=`ls -td TT_mass_1755*/ | head -n 1 | cut -d'/' -f1`  
+TMASS4=`ls -td TT_mass_1785*/ | head -n 1 | cut -d'/' -f1`  
 
+
+echo ${TTLLDIR}
+hadd hisOut_TTLL_powheg.root ${TTLLDIR}/ANA*/ANA*.root
+echo ${DY1}
+hadd histOut_DYJets_10to50.root ${DY1}/ANA*/ANA*.root
+echo ${DY2}
+hadd histOut_DYJets.root ${DY2}/ANA*/ANA*.root
+echo ${ST1}
+hadd histOut_SingleTop_tW.root ${ST1}/ANA*/ANA*.root
+echo ${ST2}
+hadd histOut_SingleTbar_tW.root ${ST2}/ANA*/ANA*.root
+echo ${TMASS1}
+hadd histOut_TT_powheg_mtop1665.root ${TMASS1}/ANA*/ANA*.root
+echo ${TMASS2}
+hadd histOut_TT_powheg_mtop1695.root ${TMASS2}/ANA*/ANA*.root
+echo ${TMASS3}
+hadd histOut_TT_powheg_mtop1755.root ${TMASS3}/ANA*/ANA*.root
+echo ${TMASS4}
+hadd histOut_TT_powheg_mtop1785.root ${TMASS4}/ANA*/ANA*.root
--- Release/submitTopMeasure.sh
+++ Release/submitTopMeasure.sh
@@ -11,7 +11,7 @@ condor_submit run.jdl
 cd ..
 
 echo "Now waiting next job for DY10to50"
-~/bin/submitCondor -o TTLL -f filelist_DY10to50.txt $1
+~/bin/submitCondor -o DY10to50 -f filelist_DY10to50.txt $1
 DY1=`ls -td DY10to50*/ | head -n 1 | cut -d'/' -f1`  
 cd ${DY1}
 condor_submit run.jdl
@@ -32,7 +32,7 @@ condor_submit run.jdl
 cd ..
 
 echo "Now waiting next job for SingleTop_tW_antitop"
-~/bin/submitCondor -o ST_tW_antitop -f filelist_ST_tW_antitop.txt $1
+~/bin/submitCondor -o ST_tW_antitop -f filelist_ST_tW_atop.txt $1
 ST2=`ls -td ST_tW_antitop*/ | head -n 1 | cut -d'/' -f1`  
 cd ${ST2}
 condor_submit run.jdl
@@ -48,20 +48,20 @@ cd ..
 echo "Now waiting next job for TT mass 169.5"
 ~/bin/submitCondor -o TT_mass_1695 -f filelist_TT_mass_1695.txt $1
 TMASS2=`ls -td TT_mass_1695*/ | head -n 1 | cut -d'/' -f1`  
-cd ${TMASS1}
+cd ${TMASS2}
 condor_submit run.jdl
 cd ..
 
 echo "Now waiting next job for TT mass 175.5"
 ~/bin/submitCondor -o TT_mass_1755 -f filelist_TT_mass_1755.txt $1
 TMASS3=`ls -td TT_mass_1755*/ | head -n 1 | cut -d'/' -f1`  
-cd ${TMASS1}
+cd ${TMASS3}
 condor_submit run.jdl
 cd ..
 
-echo "Now waiting next job for TT mass 166.5"
-~/bin/submitCondor -o TT_mass_1665 -f filelist_TT_mass_1665.txt $1
-TMASS4=`ls -td TT_mass_1665*/ | head -n 1 | cut -d'/' -f1`  
-cd ${TMASS1}
+echo "Now waiting next job for TT mass 178.5"
+~/bin/submitCondor -o TT_mass_1785 -f filelist_TT_mass_1785.txt $1
+TMASS4=`ls -td TT_mass_1785*/ | head -n 1 | cut -d'/' -f1`  
+cd ${TMASS4}
 condor_submit run.jdl
 cd ..
